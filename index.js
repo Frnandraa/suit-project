520c9dd8a3c46795289e6bb53b70d08d56fa299c
Frnandraa <frnandraa7@gmail.com>
2024-09-24 21:18:04 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var randomNumber1 = Math.floor(Math.random()*3) + 1;
+
+document.querySelector(".img1").setAttribute("src", "images/suit" + randomNumber1 + ".png");
+
+var randomNumber2 = Math.floor(Math.random()*3) + 1;
+
+document.querySelector(".img2").setAttribute("src", "images/suit" + randomNumber2 + ".png");
+
